9898268a8f53e7d860a83b214db79f24f88b38ed
Luis Hernandez <sarco_luis@hotmail.com>
2025-10-13 08:26:40 -0600

    creacion del modelo y migracion de la base de datos en sqlite

--- academica/alumno/models.py
+++ academica/alumno/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class alumno(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=150)
+    telefono = models.CharField(max_length=10)
+    email = models.CharField(max_length=75)
