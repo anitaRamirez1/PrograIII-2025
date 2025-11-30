4b27452a1c48a46901bbb8f0a8814edf0f9846be
anita <eliayala1105@gmail.com>
2025-11-30 14:24:23 -0600

    creacion de la rama api-modelo y del coversor

--- main.js
+++
@@ -1,5 +0,0 @@
-// Para usuarios - usa "usuario" (singular) que coincide con usuario.html
-window.location.href = '/vistas?form=usuario';
-
-// Para alumnos - usa "alumnos" (plural) que coincide con alumnos.html  
-window.location.href = '/vistas?form=alumnos';
--- server.py
+++ server.py
@@ -1,20 +1,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
 from urllib.parse import urlparse, parse_qs
-import json 
-import crud_alumno
-import crud_usuario
+import json
+import tensorflow as tf
+import numpy as np
 
+model = tf.keras.models.load_model('grados.h5')
 port = 3000
-
-crudAlumno = crud_alumno.crud_alumno()
-crudUsuario = crud_usuario.crud_usuario()
-
-from flask import Flask, send_file
-import os
-
-app = Flask(__name__)
-
 
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -25,21 +17,6 @@ class miServidor(SimpleHTTPRequestHandler):
         if self.path=="/":
             self.path="index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
-        if self.path=="/alumnos":
-            alumnos = crudAlumno.consultar("")
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(json.dumps(alumnos).encode('utf-8'))
-            return
-        elif path=="/usuario":
-            usuario = crudUsuario.consultar("")
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(json.dumps(usuario).encode('utf-8'))
-            return
-        elif path=="/vistas":
-            self.path = '/modulos/'+ parametros['form'][0] +'.html'
-            return SimpleHTTPRequestHandler.do_GET(self)
     
     def do_POST(self):
         longitud = int(self.headers['Content-Length'])
@@ -47,20 +24,13 @@ class miServidor(SimpleHTTPRequestHandler):
         datos = datos.decode("utf-8")
         datos = parse.unquote(datos)
         datos = json.loads(datos)
-        resp = {"msg": crudAlumno.administrar(datos)}
+        c = int(datos['celsius'])
+        prediccion = model.predict(np.array([c]))
+        resp = {"grados": str(prediccion[0][0])}
         
         self.send_response(200)
         self.end_headers()
         self.wfile.write(json.dumps(resp).encode("utf-8"))
-        
-        return
-    
-@app.route('/')
-def home():
-
-    return send_file(os.path.join(os.path.dirname(__file__), 'login.html'))
-if __name__ == '__main__':
-    app.run(debug=True) 
 
 print("Servidor ejecutandose en el puerto", port)
 server = HTTPServer(("localhost", port), miServidor)
